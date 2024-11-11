e33137b23b6b8c3cae2299f8164e1e605f22932c
Alex Patton <pattona6@mail.uc.edu>
2024-11-11 14:29:32 -0500

    closed the API and changed the search for the author's names into a search for the first sentance

--- Walleye_Assignment10/mainPackage/main.py
+++ Walleye_Assignment10/mainPackage/main.py
@@ -4,6 +4,12 @@ import json
 import requests
 
 def JSONMod():
-    response = requests.get('https://openlibrary.org/search.json?title=the+lord+of+the+rings')
+    response = requests.get('https://openlibrary.org/search.json?title=the+lord+of+the+rings') # gets the API url
+    json_string = response.content # recieves results from the URL
+    parsed_json = json.loads(json_string) #parses the results into a python dictionary
+    for firstSentance in parsed_json['docs']:
+        print (firstSentance["first_sentence"]) 
+
+JSONMod()
     
 
