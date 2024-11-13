4da44bf5b4655f5453c91aaba6b71268486596c9
Alex Patton <pattona6@mail.uc.edu>
2024-11-12 20:16:42 -0500

    Updated the main

--- Walleye_Assignment10/mainPackage/main.py
+++ Walleye_Assignment10/mainPackage/main.py
@@ -1,5 +1,35 @@
 #main.py
 
+from apiPackage.api import OpenLibraryAPIHandler
+ 
+if __name__ == "__main__":
+    print("Starting the program")
+ 
+    # Build the endpoint for searching the title "The Lord of the Rings"
+    endpoint = "search.json?title=the+lord+of+the+rings"
+    # Fetch data from the API
+    api_handler = OpenLibraryAPIHandler()
+    json_data = api_handler.fetch_data(endpoint)
+ 
+    # If data was successfully fetched, process it
+    if json_data:
+        # Extract data using the existing method
+        extracted_data = api_handler.extract_data(json_data)
+        # Save the extracted data to a CSV file (if needed)
+        api_handler.save_to_csv(extracted_data, "lord_of_the_rings_books.csv")
+    else:
+        print("No data to process.")
+ 
+    print("Program completed")
+has context menu
+
+
+has context menu
+
+
+
+
+'''
 import json 
 import requests
 
@@ -13,5 +43,5 @@ def JSONMod():
         break
 
 JSONMod()
-    
+    '''
 
