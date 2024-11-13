0a413c28c6cb178f193c9ca6da68cb98c6e02fa4
Alex Patton <pattona6@mail.uc.edu>
2024-11-12 20:22:29 -0500

    Pulled the content from the api module.

--- Walleye_Assignment10/mainPackage/main.py
+++ Walleye_Assignment10/mainPackage/main.py
@@ -21,27 +21,7 @@ if __name__ == "__main__":
         print("No data to process.")
  
     print("Program completed")
-has context menu
-
-
-has context menu
 
 
 
-
-'''
-import json 
-import requests
-
-def JSONMod():
-    response = requests.get('https://openlibrary.org/search.json?title=the+lord+of+the+rings') # gets the API url
-    json_string = response.content # recieves results from the URL
-    parsed_json = json.loads(json_string) #parses the results into a python dictionary
-
-    for firstSentance in parsed_json['docs']:
-        print (firstSentance["first_sentence"][0]) 
-        break
-
-JSONMod()
-    '''
 
